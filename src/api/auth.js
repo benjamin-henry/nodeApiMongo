b80af1effd78f0823088e4d64d8fb79ba91d07fc
unknown <benjamin.henry230292@gmail.com>
2020-08-05 16:49:25 +0200

    added login function

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,24 +1,68 @@
 const router = require('express').Router();
+let MongoClient = require("mongodb").MongoClient;
+
+router.get('/', (req, res) => {
+    res.json({
+      message: 'API - auth'
+    });
+  });
 
 router.post('/login', (req, res, next) => {
-    res.json({
-        message:'login attempt'
-    })
+    const params = req.body.params
+    MongoClient.connect(process.env.DB_URL, function(err, db) {
+        if (err) {
+            res.send({msg:"connection failed"});
+        } else {
+            connected = true;
+            let users = db.db('myAppdb').collection('users');
+            users.findOne(params, function(err, result) {
+                err ? res.send({msg:"query failed"}) 
+                : result ? res.send({user:result}) 
+                : res.send({user:null});
+            })
+            db.close()
+        }
+    }) 
 })
 
-router.post('/logout', (req, res, next) => {
-    res.json({
-        message:'logout attempt'
-    })
+router.post('/logout', (req, res, next) => {  
+    const params = req.body.params
+    MongoClient.connect(process.env.DB_URL, function(err, db) {
+        if (err) {
+            res.send({msg:"connection failed"});
+        } else {
+            connected = true;
+            let users = db.db('myAppdb').collection('users');
+            users.findOne(params, function(err, result) {
+                err ? res.send({msg:"query failed"}) 
+                : result ? res.send({user:result}) 
+                : res.send({user:null});
+            })
+            db.close()
+        }
+    }) 
 })
 
 router.post('/register', (req, res, next) => {
-    res.json({
-        message:'registerUser attempt'
+    const params = req.body.params
+    MongoClient.connect(process.env.DB_URL, function(err, db) {
+        if (err) {
+            res.send({msg:"connection failed"});
+        } else {
+            connected = true;
+            let users = db.db('myAppdb').collection('users');
+            // users.insert(params, function(err, result) {
+            //     err ? res.send({msg:err}) 
+            //     : result ? res.send({user:result}) 
+            //     : res.send({user:null});
+            // })
+            db.close()
+        }
     })
 })
 
 router.post('/update', (req, res, next) => {
+    const params = req.body.params;
     res.json({
         message:'update attempt'
     })
--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'hello'
+    message: 'hello app'
   });
 });
 
